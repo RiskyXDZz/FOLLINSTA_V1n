1c3cf32e2ecb49b02065542a1fa4ec771b8b3775
RiskyXDZz <riskisihombing829@gmail.com>
2024-10-17 22:03:25 +0700

    Add files via upload

---
+++ Penting1.sh
@@ -0,0 +1,38 @@
+clear 
+red='\033[31;1m'
+green='\033[32;1m'
+yellow='\033[33;1m' 
+
+echo $green"Ketik" $red" :" $yellow"pip instal requirements.txt"
+echo
+echo
+echo
+echo
+echo
+echo "kepentingan"
+
+echo
+echo
+echo
+echo $green"UTHOR : Risky"
+echo $green"Ig:kyy.sh21"
+sleep 2
+
+echo $green"Id Publick nih untuk orang pertama Make sc ini , buat Crack Fb"
+echo "#################################"
+echo $yellow"1000872261987655"
+sleep 1
+echo "Tanggal Sekarang"
+date
+
+echo
+echo "hshshuzu"
+echo "Syntax fourgment sinema"
+echo "############_@______@--@&@&@-+@(#@&#-++@+(())#++89@0#((##"
+echo
+sleep 1
+echo $green "Jalankan Run.py"
+echo
+echo
+clear sleep 6
+exit
